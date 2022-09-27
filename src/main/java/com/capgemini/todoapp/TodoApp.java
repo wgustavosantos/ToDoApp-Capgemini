3c544db315cfafbf97c6a1c5e61753fefdd611b3
Gustavo Santos <guto15santos@gmail.com>
2022-09-27 16:12:53 -0300

    Projeto ToDoApp iniciado

---
+++ src/main/java/com/capgemini/todoapp/TodoApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.capgemini.todoapp;
+
+import java.sql.Connection;
+import util.ConnectionFactory;
+
+/**
+ *
+ * @author WENDERSON
+ */
+public class TodoApp {
+
+    public static void main(String[] args) {
+        
+    }
+}
