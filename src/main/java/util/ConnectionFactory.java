ef11591dba28872d5102ee65fad3404246646b40
Gustavo Santos <guto15santos@gmail.com>
2022-09-27 16:42:15 -0300

    10.E criacao da conexao com o banco de dados IV / closeConnection()

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -6,6 +6,7 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -26,6 +27,17 @@ public class ConnectionFactory {
         } catch (Exception ex) {
             throw new RuntimeException("Erro na conexão com o banco de dados", ex);
         }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+
     }
 
 }
