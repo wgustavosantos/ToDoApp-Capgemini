849d4a1f76281ac6340f7276b1aa5dd898a819a2
Gustavo Santos <guto15santos@gmail.com>
2022-09-27 20:18:05 -0300

    11.C criacao dos controladores III / TasksDAO.save(Tasks tasks)

--- src/main/java/controller/TaskDAO.java
+++ src/main/java/controller/TaskDAO.java
@@ -5,6 +5,7 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -18,6 +19,45 @@ import util.ConnectionFactory;
 public class TaskDAO {
 
     public void save(Task task) {
+
+        String sql = "INSERT INTO tasks(idProject, name, description, completed "
+                + ", notes, deadline, createdAt, updatedAt) VALUES "
+                + "(?, ?, ?, ?, ?, ?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            //Cria uma conex�o com o banco
+            conn = ConnectionFactory.getConnection();
+            //Cria um PreparedStatment, classe usada para executar a query
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, task.getIdProject());
+            stmt.setString(2, task.getName());
+            stmt.setString(3, task.getDescription());
+            stmt.setBoolean(4, task.isCompleted());
+            stmt.setString(5, task.getNotes());
+            stmt.setDate(6, new Date(task.getDeadline().getTime()));
+
+            //Executa a sql para inser��o dos dados
+            stmt.execute();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao salvar a tarefa " + ex.getMessage(), ex);
+        } finally {
+            //Fecha as conex�es
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException("Erro ao fechar a conexão", ex);
+            }
+        }
+
     }
 
     public void update(Task task) {
