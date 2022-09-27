54b979f74166203271c6ce6827efd2daa60712b6
Gustavo Santos <guto15santos@gmail.com>
2022-09-27 16:55:03 -0300

    11.B criacao dos controladores II / TasksDAO.removeById()

--- src/main/java/controller/TaskDAO.java
+++ src/main/java/controller/TaskDAO.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import model.Task;
+import util.ConnectionFactory;
 
 /**
  *
@@ -31,10 +32,16 @@ public class TaskDAO {
 
         try {
 
-        } catch (Exception ex) {
+            conn = ConnectionFactory.getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.execute();
 
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao deletar a tarefa", ex);
+        } finally {
+            ConnectionFactory.closeConnection(conn);
         }
-
     }
 
     public List<Task> getAll() {
