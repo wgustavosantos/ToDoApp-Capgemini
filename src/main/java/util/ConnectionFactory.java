4d0e14a1d729750e266fffc377ee92570e995b54
Gustavo Santos <guto15santos@gmail.com>
2022-09-27 16:36:32 -0300

    10.D criacao da conexao com o banco de dados II / getConnection()

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -1,4 +1,4 @@
- /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -6,16 +6,25 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 
 /**
  *
  * @author WENDERSON
  */
 public class ConnectionFactory {
+
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
     public static final String USER = "wgroot";
     public static final String PASS = "admin";
-    
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", ex);
+        }
+    }
+
 }
