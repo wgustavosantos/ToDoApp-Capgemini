332584b2b41379bcd10d38d8801a94fbde471c9d
Gustavo Santos <guto15santos@gmail.com>
2022-09-28 15:36:52 -0300

    11.K criacao dos controladores XI / ProjectDAO / revisando controladores)

--- src/main/java/controller/ProjectDAO.java
+++ src/main/java/controller/ProjectDAO.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import model.Project;
