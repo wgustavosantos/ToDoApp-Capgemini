4aa6b1688183034eb76a3bb9c59a5189412d9406
Gustavo Santos <guto15santos@gmail.com>
2022-09-29 17:05:01 -0300

    13.B Implementando a tela de cadastro II / ProjectDialogSceen / Salvando Projeto no banco de dados pela interface

--- src/main/java/view/ProjectDialogScreen.java
+++ src/main/java/view/ProjectDialogScreen.java
@@ -15,13 +15,12 @@ import model.Project;
 public class ProjectDialogScreen extends javax.swing.JDialog {
 
     ProjectDAO projectDAO;
-    
+
     public ProjectDialogScreen(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
         projectDAO = new ProjectDAO();
-        
-        
+
     }
 
     /**
@@ -147,16 +146,20 @@ public class ProjectDialogScreen extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jLabelToolBarSaveMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelToolBarSaveMouseClicked
-        
-        Project project = new Project();
+
+        try {
+            Project project = new Project();
             project.setName(jTextFieldName.getText());
             project.setDescription(jTextAreaDescription.getText());
 
             projectDAO.save(project);
 
             JOptionPane.showMessageDialog(rootPane, "Projeto salvo com sucesso");
-            
-            this.dispose();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(rootPane, e);
+        }
+
+        this.dispose();
     }//GEN-LAST:event_jLabelToolBarSaveMouseClicked
 
     private void jTextFieldNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldNameActionPerformed
@@ -174,7 +177,7 @@ public class ProjectDialogScreen extends javax.swing.JDialog {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Java swing".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
