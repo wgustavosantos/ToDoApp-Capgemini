4f2d8775f4f9663e4699ca4ef043683ae9899283
Wenderson Santos <guto15santos@gmai.com>
2022-10-01 10:49:58 -0300

    16.A Implementando o model da lista de tarefas I/ TableTaskModel

---
+++ src/main/java/util/TaskTableModel.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.Task;
+
+/**
+ *
+ * @author WENDERSON
+ */
+public class TaskTableModel extends AbstractTableModel {
+    
+    String[] columns = {"Nome", "Descrição", "Prazo", "Tarefa Concluída", "Editar", "Concluir"};
+    List<Task> tasks = new ArrayList<>();
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
