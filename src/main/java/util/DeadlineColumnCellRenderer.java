94f691df13ac87f99b90a10e9bc686160144297b
Wenderson Santos <guto15santos@gmai.com>
2022-10-02 15:14:19 -0300

    18.A Customizações na Interface I / MainScreen

---
+++ src/main/java/util/DeadlineColumnCellRenderer.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.Date;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import model.Task;
+
+/**
+ *
+ * @author WENDERSON
+ */
+public class DeadlineColumnCellRenderer extends DefaultTableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+        //Cells are by default rendered as a JLabel.
+        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value,
+                isSelected, hasFocus, row, col);
+        label.setHorizontalAlignment( JLabel.CENTER );
+
+      
+        return null;
+    }
