f99b4f80c780d6078eaeb256c1236fac66fcf19b
Wenderson Santos <guto15santos@gmai.com>
2022-10-02 10:31:15 -0300

    17.G Finalizando as funcionalidades básicas VII / MainScreen / Atualizando Tasks ao adicionar uma Tarefa

--- src/main/java/view/MainScreen.java
+++ src/main/java/view/MainScreen.java
@@ -355,6 +355,15 @@ public class MainScreen extends javax.swing.JFrame {
         taskDialogScreen.setProject(project);
         
         taskDialogScreen.setVisible(true);
+        
+        
+            taskDialogScreen.addWindowListener(new WindowAdapter() {
+                public void windowClosed(WindowEvent e) {
+                    int projectIndex = jListProjects.getSelectedIndex();
+                    Project project = (Project) projectModel.get(projectIndex);
+                    loadTasks(project.getId());
+                }
+            });
     }//GEN-LAST:event_jLabelTasksAddMouseClicked
 
     private void jTableTasksMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableTasksMouseClicked
